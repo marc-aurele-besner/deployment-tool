c857958578cd7ec0f386c386a76c776449e67dc8
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2022-09-29 16:27:13 -0400

    add switch for network name

--- src/deployProxy.ts
+++ src/deployProxy.ts
@@ -37,96 +37,103 @@ const deployProxy = async (
         let ProxyAdminAddress: string = ''
 
         while (keepWaiting) {
-            // Get deployer account
-            const [deployer] = await env.ethers.getSigners()
+            switch (env.network.name) {
+                case 'tronex': {
+                    console.log('Deploying to Tronex')
+                }
+                default: {
+                    // Get deployer account
+                    const [deployer] = await env.ethers.getSigners()
 
-            // Make sure contract is compiled
-            await compileContract(env)
+                    // Make sure contract is compiled
+                    await compileContract(env)
 
-            // Get Interface
-            const contractInterface = await env.ethers.getContractFactory(contractName)
+                    // Get Interface
+                    const contractInterface = await env.ethers.getContractFactory(contractName)
 
-            // Deploy Proxy & initialize it
-            deployedContract = await env.upgrades.deployProxy(contractInterface, initializeArguments, {
-                initializer: initializeSignature
-            })
+                    // Deploy Proxy & initialize it
+                    deployedContract = await env.upgrades.deployProxy(contractInterface, initializeArguments, {
+                        initializer: initializeSignature
+                    })
 
-            // Get Transaction Receipt
-            const deployedContractTnx = await deployedContract.deployTransaction.wait()
+                    // Get Transaction Receipt
+                    const deployedContractTnx = await deployedContract.deployTransaction.wait()
 
-            // Save deployment arguments
-            const extraData = {
-                ...extra,
-                initializeArguments,
-                initializeSignature
-            }
+                    // Save deployment arguments
+                    const extraData = {
+                        ...extra,
+                        initializeArguments,
+                        initializeSignature
+                    }
 
-            // Save the deployment details
-            await env.addressBook.saveContract(
-                contractName,
-                deployedContract.address,
-                env.network.name,
-                deployer.address,
-                deployedContractTnx.blockHash,
-                deployedContractTnx.blockNumber,
-                tag,
-                extraData
-            )
-            logOutput.push({
-                contractName,
-                address: deployedContract.address,
-                network: env.network.name
-            })
-
-            // Console log the address
-            console.log('\x1b[32m%s\x1b[0m', `${contractName} deployed at address: `, deployedContract.address)
-
-            // Retrieve Proxy Admin Address
-            ProxyAdminAddress = await env.addressBook.retrieveOZAdminProxyContract(env.network.config.chainId)
-
-            // Save Proxy Admin Address
-            env.addressBook.saveContract('ProxyAdmin', ProxyAdminAddress, env.network.name, deployer.address)
-            logOutput.push({
-                contractName: 'ProxyAdmin',
-                address: ProxyAdminAddress,
-                network: env.network.name
-            })
-
-            // Console log the address
-            console.log('Deployed using Proxy Admin contract address: ', ProxyAdminAddress)
-
-            // Verify the contract
-            if (verifyContract) await etherscanVerifyContract(env, deployedContract.address)
-
-            if (!skipGit) {
-                // Add the contract address files and contract storage layout to the next commit
-                const filesToCommit = `.openzeppelin/ contractsAddressDeployed.json contractsAddressDeployedHistory.json`
-                const isAddedToCommit = await addToCommit(filesToCommit)
-                let isCommitted = false
-
-                // Get last CommitId
-                const lastCommit = await getLastCommit()
-
-                // Commit
-                if (isAddedToCommit && lastCommit.success)
-                    isCommitted = await commitChanges(
-                        `🆕 ${contractName} deployed from commitId: ${lastCommit.commitId}`,
-                        `Network: ${env.network.name}, Deployer: ${deployer.address}, Contract Address: ${deployedContract.address}, Initialize Arguments: ${JSON.stringify(initializeArguments)}, Initialize Signature: ${initializeSignature}, Proxy Admin Address: ${ProxyAdminAddress}`,
-                        filesToCommit
+                    // Save the deployment details
+                    await env.addressBook.saveContract(
+                        contractName,
+                        deployedContract.address,
+                        env.network.name,
+                        deployer.address,
+                        deployedContractTnx.blockHash,
+                        deployedContractTnx.blockNumber,
+                        tag,
+                        extraData
                     )
-                let isPull = false
+                    logOutput.push({
+                        contractName,
+                        address: deployedContract.address,
+                        network: env.network.name
+                    })
 
-                // Pull
-                if (isCommitted) isPull = await pullFromGit()
+                    // Console log the address
+                    console.log('\x1b[32m%s\x1b[0m', `${contractName} deployed at address: `, deployedContract.address)
 
-                // Push
-                if (isPull) await pushToGit(filesToCommit)
-            } else console.log('Skipping git commit, pull & push')
-            // Exit
-            keepWaiting = false
+                    // Retrieve Proxy Admin Address
+                    ProxyAdminAddress = await env.addressBook.retrieveOZAdminProxyContract(env.network.config.chainId)
 
-            // Return the deployed contract and Proxy Admin
-            if (logOutput.length > 0) console.table(logOutput)
+                    // Save Proxy Admin Address
+                    env.addressBook.saveContract('ProxyAdmin', ProxyAdminAddress, env.network.name, deployer.address)
+                    logOutput.push({
+                        contractName: 'ProxyAdmin',
+                        address: ProxyAdminAddress,
+                        network: env.network.name
+                    })
+
+                    // Console log the address
+                    console.log('Deployed using Proxy Admin contract address: ', ProxyAdminAddress)
+
+                    // Verify the contract
+                    if (verifyContract) await etherscanVerifyContract(env, deployedContract.address)
+
+                    if (!skipGit) {
+                        // Add the contract address files and contract storage layout to the next commit
+                        const filesToCommit = `.openzeppelin/ contractsAddressDeployed.json contractsAddressDeployedHistory.json`
+                        const isAddedToCommit = await addToCommit(filesToCommit)
+                        let isCommitted = false
+
+                        // Get last CommitId
+                        const lastCommit = await getLastCommit()
+
+                        // Commit
+                        if (isAddedToCommit && lastCommit.success)
+                            isCommitted = await commitChanges(
+                                `🆕 ${contractName} deployed from commitId: ${lastCommit.commitId}`,
+                                `Network: ${env.network.name}, Deployer: ${deployer.address}, Contract Address: ${deployedContract.address}, Initialize Arguments: ${JSON.stringify(initializeArguments)}, Initialize Signature: ${initializeSignature}, Proxy Admin Address: ${ProxyAdminAddress}`,
+                                filesToCommit
+                            )
+                        let isPull = false
+
+                        // Pull
+                        if (isCommitted) isPull = await pullFromGit()
+
+                        // Push
+                        if (isPull) await pushToGit(filesToCommit)
+                    } else console.log('Skipping git commit, pull & push')
+                    // Exit
+                    keepWaiting = false
+
+                    // Return the deployed contract and Proxy Admin
+                    if (logOutput.length > 0) console.table(logOutput)
+                }
+            }
         }
         // Return the deployed contract{
         return {
--- src/upgradeProxy.ts
+++ src/upgradeProxy.ts
@@ -35,82 +35,89 @@ const upgradeProxy = async (
         let ProxyAdminAddress: string = ''
 
         while (keepWaiting) {
-            // Get deployer account
-            const [deployer] = await env.ethers.getSigners()
+            switch (env.network.name) {
+                case 'tronex': {
+                    console.log('Deploying to Tronex')
+                }
+                default: {
+                    // Get deployer account
+                    const [deployer] = await env.ethers.getSigners()
 
-            // Make sure contract is compiled
-            await compileContract(env)
+                    // Make sure contract is compiled
+                    await compileContract(env)
 
-            // Get Interface
-            const contractInterface = await env.ethers.getContractFactory(contractName)
+                    // Get Interface
+                    const contractInterface = await env.ethers.getContractFactory(contractName)
 
-            // Get contract details
-            const contractAddress = await env.addressBook.retrieveContract(contractName, env.network.name)
+                    // Get contract details
+                    const contractAddress = await env.addressBook.retrieveContract(contractName, env.network.name)
 
-            // Sanity check on contract address
-            if (!contractAddress) throw new Error(`Contract ${contractName} not found in address book`)
-            
-            // Deploy Proxy & initialize it
-            upgradedContract = await env.upgrades.upgradeProxy(contractAddress, contractInterface)
+                    // Sanity check on contract address
+                    if (!contractAddress) throw new Error(`Contract ${contractName} not found in address book`)
+                    
+                    // Deploy Proxy & initialize it
+                    upgradedContract = await env.upgrades.upgradeProxy(contractAddress, contractInterface)
 
-            // Get Transaction Receipt
-            const upgradedContractTnx = await upgradedContract.deployTransaction.wait()
+                    // Get Transaction Receipt
+                    const upgradedContractTnx = await upgradedContract.deployTransaction.wait()
 
-            // Save the deployment details
-            await env.addressBook.saveContract(
-                contractName,
-                upgradedContract.address,
-                env.network.name,
-                deployer.address,
-                upgradedContractTnx.blockHash,
-                upgradedContractTnx.blockNumber,
-                tag,
-                extra
-            )
-            logOutput.push({
-                contractName,
-                address: upgradedContract.address,
-                network: env.network.name
-            })
-
-            // Console log the address
-            console.log('\x1b[32m%s\x1b[0m', `${contractName} upgraded at address: `, upgradedContract.address)
-
-            // Retrieve Proxy Admin Address
-            ProxyAdminAddress = await env.addressBook.retrieveOZAdminProxyContract(env.network.config.chainId)
-
-            // Verify the contract
-            if (verifyContract) await etherscanVerifyContract(env, upgradedContract.address)
-
-            if (!skipGit) {
-                // Add the contract address files and contract storage layout to the next commit
-                const filesToCommit = `.openzeppelin/ contractsAddressDeployed.json contractsAddressDeployedHistory.json`
-                const isAddedToCommit = await addToCommit(filesToCommit)
-                let isCommitted = false
-
-                // Get last CommitId
-                const lastCommit = await getLastCommit()
-
-                // Commit
-                if (isAddedToCommit && lastCommit.success)
-                    isCommitted = await commitChanges(
-                        `💪 ${contractName} upgraded from commitId: ${lastCommit.commitId}`,
-                        `Network: ${env.network.name}, Deployer: ${deployer.address}, Contract Address: ${upgradedContract.address}${ProxyAdminAddress ? ', Proxy Admin Address: ' + ProxyAdminAddress : ''}`,
-                        filesToCommit
+                    // Save the deployment details
+                    await env.addressBook.saveContract(
+                        contractName,
+                        upgradedContract.address,
+                        env.network.name,
+                        deployer.address,
+                        upgradedContractTnx.blockHash,
+                        upgradedContractTnx.blockNumber,
+                        tag,
+                        extra
                     )
-                let isPull = false
+                    logOutput.push({
+                        contractName,
+                        address: upgradedContract.address,
+                        network: env.network.name
+                    })
 
-                // Pull
-                if (isCommitted) isPull = await pullFromGit()
+                    // Console log the address
+                    console.log('\x1b[32m%s\x1b[0m', `${contractName} upgraded at address: `, upgradedContract.address)
 
-                // Push
-                if (isPull) await pushToGit(filesToCommit)
-            } else console.log('Skipping git commit, pull & push')
-            // Exit
-            keepWaiting = false
+                    // Retrieve Proxy Admin Address
+                    ProxyAdminAddress = await env.addressBook.retrieveOZAdminProxyContract(env.network.config.chainId)
 
-            // Return the deployed contract and Proxy Admin
-            if (logOutput.length > 0) console.table(logOutput)
+                    // Verify the contract
+                    if (verifyContract) await etherscanVerifyContract(env, upgradedContract.address)
+
+                    if (!skipGit) {
+                        // Add the contract address files and contract storage layout to the next commit
+                        const filesToCommit = `.openzeppelin/ contractsAddressDeployed.json contractsAddressDeployedHistory.json`
+                        const isAddedToCommit = await addToCommit(filesToCommit)
+                        let isCommitted = false
+
+                        // Get last CommitId
+                        const lastCommit = await getLastCommit()
+
+                        // Commit
+                        if (isAddedToCommit && lastCommit.success)
+                            isCommitted = await commitChanges(
+                                `💪 ${contractName} upgraded from commitId: ${lastCommit.commitId}`,
+                                `Network: ${env.network.name}, Deployer: ${deployer.address}, Contract Address: ${upgradedContract.address}${ProxyAdminAddress ? ', Proxy Admin Address: ' + ProxyAdminAddress : ''}`,
+                                filesToCommit
+                            )
+                        let isPull = false
+
+                        // Pull
+                        if (isCommitted) isPull = await pullFromGit()
+
+                        // Push
+                        if (isPull) await pushToGit(filesToCommit)
+                    } else console.log('Skipping git commit, pull & push')
+                    // Exit
+                    keepWaiting = false
+
+                    // Return the deployed contract and Proxy Admin
+                    if (logOutput.length > 0) console.table(logOutput)
+                }
+            }
         }
         // Return the upgraded contract
         return {
